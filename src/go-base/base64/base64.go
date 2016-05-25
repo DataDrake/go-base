068ce8a908e52b8c5c522ceddcd0b2c7dbf33d77
Bryan T. Meyers <btmeme@rit.edu>
2016-05-25 08:48:03 -0400

    ADDED: basic implementation of base64

---
+++ src/go-base/base64/base64.go
@@ -0,0 +1,42 @@
+package main
+import (
+	"os"
+	"encoding/base64"
+	"io"
+	"log"
+	"fmt"
+	"flag"
+)
+
+func main() {
+	var err error
+	decode := flag.Bool("d",false,"decode data")
+	flag.Parse()
+
+	args := flag.Args()
+
+	input := os.Stdin
+	switch len(args) {
+	case 0:
+	case 1:
+		input,err = os.Open(args[0])
+		if err != nil{
+			log.Fatal(err.Error())
+		}
+	default:
+		log.Fatalln("Usage: base64 [OPTION]... [FILE]")
+	}
+
+	if *decode {
+		dec := base64.NewDecoder(base64.StdEncoding, input)
+		_, err = io.Copy(os.Stdout, dec)
+	} else {
+		enc := base64.NewEncoder(base64.StdEncoding, os.Stdout)
+		_, err = io.Copy(enc, input)
+		enc.Close()
+		fmt.Println()
+	}
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
