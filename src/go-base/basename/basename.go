16fa06a3f87879f1b45eaf7aa2ce45c4fd30968e
Bryan T. Meyers <btmeme@rit.edu>
2016-05-25 22:26:42 -0400

    FIXED: formatting for basename FIXED: not using exit() for error states

--- src/go-base/basename/basename.go
+++ src/go-base/basename/basename.go
@@ -1,24 +1,26 @@
 package main
+
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
-func usage(){
+func usage() {
 	fmt.Println("Usage: basename NAME [SUFFIX]")
 	fmt.Println("Usage: basename [OPTIONS]... NAME...")
 	flag.PrintDefaults()
 }
 
-func getBase(path string,suffix string, zero bool) {
+func getBase(path string, suffix string, zero bool) {
 	base := filepath.Base(path)
 	if len(suffix) > 0 {
-		base = strings.TrimSuffix(base,suffix)
+		base = strings.TrimSuffix(base, suffix)
 	}
 	if zero {
-		fmt.Printf("%s\000",base)
+		fmt.Printf("%s\000", base)
 	} else {
 		fmt.Println(base)
 	}
@@ -26,24 +28,24 @@ func getBase(path string,suffix string, zero bool) {
 
 func main() {
 
-	flag.Usage = func() {usage()}
-	multiple := flag.Bool("a",false,"support multiple arguments and treat each as a NAME")
-	suffix := flag.String("s","","remove a trailing SUFFIX; implies -a")
-	zero := flag.Bool("z",false,"end each output line with NUL, not newline")
+	flag.Usage = func() { usage() }
+	multiple := flag.Bool("a", false, "support multiple arguments and treat each as a NAME")
+	suffix := flag.String("s", "", "remove a trailing SUFFIX; implies -a")
+	zero := flag.Bool("z", false, "end each output line with NUL, not newline")
 	flag.Parse()
 
-	args :=flag.Args()
+	args := flag.Args()
 
 	if len(args) == 0 {
 		usage()
-		return
+		os.Exit(1)
 	}
 
 	if *multiple || len(*suffix) > 0 {
-		for _,v := range args {
-			getBase(v,*suffix,*zero)
+		for _, v := range args {
+			getBase(v, *suffix, *zero)
 		}
 	} else {
-		getBase(args[0],*suffix,*zero)
+		getBase(args[0], *suffix, *zero)
 	}
 }
